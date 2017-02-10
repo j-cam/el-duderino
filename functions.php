5f291fd2a82746dbbc1ab27bb512c3c16141c376
Jamie Campbell <jamie@spiritedcreative.com>
2017-02-09 19:43:57 -0800

    Lots of functions updates

--- functions.php
+++ functions.php
@@ -42,10 +42,61 @@ function el_duderino_setup() {
 	 */
 	add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
+	// This theme uses wp_nav_menu() in three locations.
 	register_nav_menus( array(
-		'menu-1' => esc_html__( 'Primary', 'el-duderino' ),
+		'primary' => esc_html__( 'Primary', 'el-duderino' ),
+		'primary-mobile' => esc_html__( 'Primary Mobile', 'el-duderino' ),
+		'footer' => esc_html__( 'Footer', 'el-duderino' ),
 	) );
+
+
+
+
+	if ( ! function_exists( 'get_nav_menu_item_count' ) ) {
+
+			function get_nav_menu_item_count( $location ) {
+
+				$menu_to_count = wp_get_nav_menu_items(array(
+					'echo' => 0,
+					'depth' => 1,
+					'theme_location' => $location
+					));
+
+				$count = substr_count($menu_to_count,'class="menu-item ');
+
+				return $count;
+
+			}
+
+	}
+
+
+	if ( ! function_exists( 'get_nav_menu_item_count_class' ) ) {
+
+			// @param string $menu [Menu name, ID, or slug]
+			function get_nav_menu_item_count_class($menu){
+
+				$menu_items = wp_get_nav_menu_items( 'primary-menu' );
+
+
+				$item_count = substr_count($menu_items,'class="menu-item ');
+
+
+				if( $item_count > 0 ) {
+
+					return ' items-'.$item_count;
+
+				}
+
+				else {
+
+					return false;
+
+				}
+
+			}
+
+	}
 
 	/*
 	 * Switch default core markup for search form, comment form, and comments
@@ -107,15 +158,91 @@ add_action( 'widgets_init', 'el_duderino_widgets_init' );
 function el_duderino_scripts() {
 	wp_enqueue_style( 'el-duderino-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'el-duderino-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script('jquery');
 
-	wp_enqueue_script( 'el-duderino-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'el-duderino-global-js', get_template_directory_uri() . '/js/global.js', array('jquery'), '1.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+
+	/**
+	 * Add async attributes to enqueued scripts where needed.
+	 * The ability to filter script tags was added in WordPress 4.1 for this purpose.
+	 */
+	// function el_duderino_async_scripts( $tag, $handle, $src ) {
+	//     // the handles of the enqueued scripts we want to async
+	//     $async_scripts = array( 'el-duderino-global-js' );
+
+	//     if ( in_array( $handle, $async_scripts ) ) {
+	//         return '<script id="' . $handle . '" type="text/javascript" src="' . $src . '" async="async"></script>' . "\n";
+	//     }
+
+	//     return $tag;
+	// }
+	// add_filter( 'script_loader_tag', 'el_duderino_async_scripts', 10, 3 );
+
+
+
+
 }
 add_action( 'wp_enqueue_scripts', 'el_duderino_scripts' );
+
+
+/**
+ * Move WP core jQuery to the footer
+ */
+// wp_scripts()->add_data( 'jquery', 'group', 1 );
+// wp_scripts()->add_data( 'jquery-core', 'group', 1 );
+// wp_scripts()->add_data( 'jquery-migrate', 'group', 1 );
+
+/**---------------------------
+ * Adobe Typekit -- Optional
+ * ---------------------------/
+
+/* ======================================
+add_action( 'wp_head', 'el_duderino_load_typekit', 0 );
+
+if ( ! function_exists( 'el_duderino_load_typekit' ) ) {
+
+function el_duderino_load_typekit() { ?>
+  <script>
+    (function(d) {
+      var config = {
+        kitId: 'nht4bzh',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
+  </script>
+<?php }
+
+}
+========================================== */
+
+
+
+
+/*=====  End of SCRIPTS & STYLES  ======*/
+
+/**
+ * Implement custom meta tags in the document head.
+ */
+require get_template_directory() . ('/inc/wp-head-meta.php');
+
+
+/**
+ * Implement the Custom functions used for Advanced Custom Fields .
+ */
+require get_template_directory() . ('/inc/acf/acf-functions.php');
+
+/**
+ * Implement the Custom menu using BEM CSS naming conventions.
+ */
+require get_template_directory() . ('/inc/wp-bem-menu.php');
+
 
 /**
  * Implement the Custom Header feature.
