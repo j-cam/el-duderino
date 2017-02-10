4d59349307be2546e2fb53cab0761f78b2f09367
Jamie Campbell <jamie@spiritedcreative.com>
2017-02-09 19:40:22 -0800

    Move typekit from meta into functions scripts

--- inc/wp-head-meta.php
+++ inc/wp-head-meta.php
@@ -42,22 +42,3 @@ add_action( 'wp_head', 'dns_prefetch', 0 );
 
 
 /*=====  End of WP HEAD / DOC HEAD / META  ======*/
-
-
-
-add_action( 'wp_head', 'advanced_typekit_loading', 0 );
-
-function advanced_typekit_loading() {
-?>
-  <script>
-    (function(d) {
-      var config = {
-        kitId: 'nht4bzh',
-        scriptTimeout: 3000,
-        async: true
-      },
-      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-    })(document);
-  </script>
-<?php
-}
