981b6de776af15a69c691cca34e5bcc0ebd523fa
Jamie Campbell <jamie@spiritedcreative.com>
2017-02-14 13:40:27 -0800

    Import mobile nav as a partial

--- header.php
+++ header.php
@@ -23,9 +23,8 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'underscores-master' ); ?></a>
 	<!-- ADD NAV DRAWER HERE  -->
-	<div id="NavDrawer" class="drawer drawer--left">
-	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'main-nav__main-menu', 'container' => false ) ); ?>
-	</div>
+	<?php locate_template( 'template-parts/_partials/mobile-nav.php', true ); ?>
+
 	<header id="masthead" class="site-header" role="banner">
 
 
@@ -62,8 +61,7 @@
 			    <div class="mobile-nav__header flex-container">
 			        <div class="site-nav--mobile">
             			<!-- _S toggle button -->
-            			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'underscores-master' ); ?></button>
-			            <button type="button" class="hamburger-button js-drawer-open-left" aria-controls="NavDrawer" aria-expanded="false">
+			            <button type="button" class="hamburger-button js-drawer-open-left" aria-controls="NavDrawer" aria-expanded="false" aria-label="<?php esc_html_e( 'Primary Menu', 'underscores-master' ); ?>">
 			                <img src="<?php echo get_template_directory_uri(); ?>/images/hamburger.svg" alt="menu" />
 			            </button>
 			        </div>
@@ -79,65 +77,3 @@
 	</header><!-- #masthead -->
 
 	<div id="content" class="layer__site-content site-content">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php /*
-			<div id="wrap-site-nav-menus" class="wrapper">
-			    <!-- start: device helper DT nav -->
-			    <div id="" class="tablet-down--hide">
-						<div class="navbar__flex-container">
-							<div class="navbar__logo-block">
-								<h1 class="site-title navbar__site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-							</div>
-						 	<div class="navbar__nav-block">
-							<?php
-								// bem_nav_menu( array(
-								// 	'theme_location' => 'primary',
-								// 	'container' => false,
-								// 	'menu_id'=> 'main-navigation',
-								// 	'menu_class' => 'main-navigation',
-								// 	'css_class_prefix' => 'primary-menu',
-								// 	'css_class_modifiers' => null
-								// 	) );
-								?>
-							</div>
-						</div>
-					</div><!-- end: device helper DT nav -->
-					<div class="desktop--hide tablet-down--show">
-						<?php
-							bem_nav_menu( array(
-								'theme_location' => 'primary-mobile',
-								'container' => false,
-								'menu_id'=> 'primary-menu-mobile',
-								'menu_class' => 'mobile-nav'. $menu_item_count_class,
-								'css_class_prefix' => 'mobile-nav',
-								'css_class_modifiers' => null
-								) );
-				    ?>
-					</div>
-*/ ?>
